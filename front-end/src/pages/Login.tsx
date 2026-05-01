7492daabda9bd27ce8f26e208808d56b97015750
Everton Sanches <74841165+EvertonSanches@users.noreply.github.com>
2026-05-01 13:55:22 -0300

    Estrutura Paginas Login e Home
    Criado estrutura e rotas entre as páginas de Login e Home

---
+++ front-end/src/pages/Login.tsx
@@ -0,0 +1,16 @@
+function Login() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <input type="email" placeholder="Email" />
+            <br />
+            
+            <input type="password" placeholder="Senha" />
+            <br />
+
+            <button>Entrar</button>
+        </div>
+    )
+}
+
+export default Login
